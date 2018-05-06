d36e8426050ffc93817552d342bf463d5149df52
Surya Prashanth <prashantsurya@ymail.com>
2018-05-06 17:02:20 +0530

    Add test for addVersion method in service

--- src/test/java/com/jiwhiz/demo/service/DemoServiceImTest.java
+++ src/test/java/com/jiwhiz/demo/service/DemoServiceImTest.java
@@ -31,16 +31,27 @@ public class DemoServiceImTest {
 
     @Autowired
 	JdbcTemplate jdbcTemplate;
-	RowMapper<Demo> rm;
+    RowMapper<Demo> person;
+    RowMapper<Demo> version;
+
 	@Before
     public void setUp() {
-        rm = new RowMapper<Demo>() {
+        person = new RowMapper<Demo>() {
             @Override
             public Demo mapRow(ResultSet resultSet, int i) throws SQLException {
                 Demo user = new Demo(resultSet.getInt("id"), resultSet.getString("name"),
                         resultSet.getString("description"), resultSet.getString("version"));
 
                 return user;
+            }
+        };
+        version = new RowMapper<Demo>() {
+            @Override
+            public Demo mapRow(ResultSet resultSet, int i) throws SQLException {
+                Demo user1 = new Demo(resultSet.getInt("service_id"), resultSet.getString("version_id"),
+                        resultSet.getString("version_status"), resultSet.getDate("version_started"));
+
+                return user1;
             }
         };
     }
@@ -52,9 +63,9 @@ public class DemoServiceImTest {
     }
 
     @Test
-    public void testAddService() throws Exception {
+    public void testAddService(){
         demoService.addService("test_name", "test_description", "1.0.0");
-        List<Demo> result = jdbcTemplate.query("select * from person where name='test_name'", rm);
+        List<Demo> result = jdbcTemplate.query("select * from person where name='test_name'", person);
         assertEquals(result.size(), 1);
         Demo demo = result.get(0);
         assertEquals(demo.name, "test_name");
@@ -62,8 +73,22 @@ public class DemoServiceImTest {
         assertEquals(demo.description, "test_description");
     }
 
+    @Test
+    public void testAddVersion(){
+        demoService.addService("test_name", "test_description", "1.0.0");
+        List<Demo> services = jdbcTemplate.query("select * from person where name='test_name'", person);
+        Demo service = services.get(0);
+        demoService.addVersion(Integer.toString(service.id), "version_id", "status", "2018-05-16");
+        String query = "select * from version where service_id='" + Integer.toString(service.id) + "'";
+        List<Demo> result = jdbcTemplate.query(query, version);
+        assertEquals(result.size(), 1);
+        Demo version = result.get(0);
+        assertEquals(version.version_id, "version_id");
+    }
+
     @After
     public void tearDown() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "version");
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "person");
     }
 
